983024e4c0e295f311b7095f13b14fb2d1997d78
Evgenii Vedegis <tichoo@yandex.ru>
2021-02-18 21:32:01 +0800

    added images

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md-body">
     <div class="card__wrapper" v-for="item in items" :key="item.id">
-      <Card :name="`${item.lvl} lvl`" :title="item.title">
+      <Card :name="`${item.lvl} lvl`" :title="item.title" :imgUrl="item.img">
         {{ item.descr }}
       </Card>
     </div>
@@ -10,6 +10,7 @@
 
 <script>
 import Card from '@/components/UI/Card'
+
 export default {
   components: { Card },
   data() {
@@ -19,12 +20,14 @@ export default {
           id: 1,
           title: 'Archer',
           descr: 'archer archer archer archer',
+          img: require('@/assets/img/archer.png'),
           lvl: 4
         },
         {
           id: 2,
           title: 'Wizard',
           descr: 'Wizard Wizard Wizard Wizard',
+          img: require('@/assets/img/wizard.png'),
           lvl: 5
         }
       ]
