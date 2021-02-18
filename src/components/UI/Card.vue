39a51c50d45b9b16060e6d3cd88e5474dcbb5beb
Evgenii Vedegis <tichoo@yandex.ru>
2021-02-18 20:18:16 +0800

    added UI/card

---
+++ src/components/UI/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <span class="card-name">{{ name }}</span>
+    <span class="card-title">{{ title }}</span>
+    <div class="card-body">
+      card-body
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,5 +2,33 @@
   <div class="md-body">
     <h1>hello home</h1>
     <p>Edit me</p>
+    <div class="card__wrapper" v-for="item in items" :key="item.id">
+      <Card :name="`${item.lvl} lvl`" :title="item.title" />
+    </div>
   </div>
 </template>
+
+<script>
+import Card from '@/components/UI/Card'
+export default {
+  components: { Card },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: 'Archer',
+          descr: 'archer archer archer archer',
+          lvl: 4
+        },
+        {
+          id: 2,
+          title: 'Wizard',
+          descr: 'Wizard Wizard Wizard Wizard',
+          lvl: 5
+        }
+      ]
+    }
+  }
+}
+</script>
