5caebb5028f7928d645ff5bd3cccea3daeba1dfd
Evgenii Vedegis <tichoo@yandex.ru>
2021-04-26 07:22:24 +0800

    added seeders

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -9,28 +9,15 @@
 </template>
 
 <script>
+import items from '@/seeders/items.js'
+
 import Card from '@/components/UI/Card'
 
 export default {
   components: { Card },
   data() {
     return {
-      items: [
-        {
-          id: 1,
-          title: 'Archer',
-          descr: 'archer archer archer archer',
-          img: require('@/assets/img/archer.png'),
-          lvl: 4
-        },
-        {
-          id: 2,
-          title: 'Wizard',
-          descr: 'Wizard Wizard Wizard Wizard',
-          img: require('@/assets/img/wizard.png'),
-          lvl: 5
-        }
-      ]
+      items: items
     }
   }
 }
---
+++ src/seeders/items.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: 'Archer',
+    descr: 'archer archer archer archer',
+    img: require('@/assets/img/archer.png'),
+    lvl: 4
+  },
+  {
+    id: 2,
+    title: 'Wizard',
+    descr: 'Wizard Wizard Wizard Wizard',
+    img: require('@/assets/img/wizard.png'),
+    lvl: 5
+  },
+  {
+    id: 3,
+    title: 'Goblin',
+    descr: 'goblin goblingo blingoblin goblingoblin',
+    img: require('@/assets/img/goblin.png'),
+    lvl: 5
+  }
+]
